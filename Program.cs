8002e3560f20daae3fbd7e564eb027d7253ccc23
andskraa <anders.skraamesto@stud.noroff.no>
2022-11-21 22:15:02 +0100

    [Base functionality for the random number generator]

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+
+namespace RandomNumbers
+{
+    class RandomNumbers
+    {
+        static void Main(string[] args)
+        {
+            
+            var generator = new RandomGenerator();
+            var nums = generator.RandomNumbers(5, 2, 55);
+            Console.Write($"Random numbers between 2 and 5: {string.Join(" ", nums)}");
+        }
+    }
+
+    class RandomGenerator
+    {
+        private readonly Random random = new Random();
+
+        public List<int> RandomNumbers(int num, int min, int max)
+        {
+            List<int> nums = new List<int>();
+            for (int i = 0; i < num; i++)
+            {
+                nums.Add(random.Next(min, max));
+            }
+
+            return nums;
+        }
+    }
+}
+
